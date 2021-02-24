3ac690421f9c5cadbf2bfbbb0f56a4b108fa2ecf
Renan Mendonca <renan.mendonca@smn.com.br>
2021-02-24 16:22:56 -0300

    Task, Pagination and Filter

--- SmartSchool.WebAPI/V1/Controllers/AlunoController.cs
+++ SmartSchool.WebAPI/V1/Controllers/AlunoController.cs
@@ -6,6 +6,8 @@ using Microsoft.EntityFrameworkCore;
 using SmartSchool.WebAPI.Data;
 using SmartSchool.WebAPI.V1.Dtos;
 using SmartSchool.WebAPI.models;
+using System.Threading.Tasks;
+using SmartSchool.WebAPI.Helpers;
 
 namespace SmartSchool.WebAPI.V1.Controllers
 {
@@ -37,10 +39,15 @@ namespace SmartSchool.WebAPI.V1.Controllers
         /// </sumary>
         /// <returns></returns>
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> Get([FromQuery]PageParams pageParams)
         {
-            var alunos = _repo.GetAllAlunos(true);  
-            return Ok(_mapper.Map<IEnumerable<AlunoDto>>(alunos));
+            var alunos = await _repo.GetAllAlunosAsync(pageParams, true);  
+
+            var alunosResult = _mapper.Map<IEnumerable<AlunoDto>>(alunos);
+
+            Response.AddPagination(alunos.CurrentPage, alunos.PageSize, alunos.TotalCount, alunos.TotalPages);
+
+            return Ok(alunosResult);
         }
 
         /// <sumary>
